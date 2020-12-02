e274785f2b1caf5dbd0c24689aa50809c9bd64ce
Jane <1129133995@qq.com>
2020-12-02 17:32:44 +0800

    js DOM

--- src/runoob/JavaScript/testdom.js
+++ src/runoob/JavaScript/testdom.js
@@ -11,6 +11,10 @@
 * 1。通过id找到HTML元素；
 * 2。通过标签名找到HTML元素；
 * 3。通过类名找到HTML元素。
+* HTML DOM使JS有能力对HTML事件作出反应：
+* 我们可以在事件发生时执行JS，比如当用户在HTML元素上点击时：onclick=JavaScript
+* HTML事件：当用户点击鼠标时、当网页已加载时、当图像已加载时、当鼠标移动到元素上时、当输入字段被改变时、当提交HTML表单时、当用户触发按键时；
+*
 * */
 
 //测试查找P元素修改 文字
@@ -30,4 +34,29 @@ function testdomlookhtml(index) {
     }
 }
 
+function changeMyText(haha) {
+    haha.innerHTML = "你好，很高兴认识你！";
+}
 
+function changeMyAnotherText(haha) {
+    haha.innerHTML = "嗯嗯，我也很高兴认识你！";
+}
+
+// function changMyhahaText(haha) {
+//     haha.innerHTML = "哈哈，我也是哦";
+// }
+
+function inputchange(input) {
+    console.log("我输入了");
+    console.log(input.value); //获取输入框文字用value属性
+}
+
+function myOmver(div) {
+    div.innerHTML = "移动到里面了";
+    div.style.backgroundColor = "red";
+}
+
+function myOut(div) {
+    div.innerHTML = "移动出去了";
+    div.style.backgroundColor = "green";
+}
