b8d8ded91c5b585c9fa11dd572a880e97f178b9f
misschen <1129133995@qq.com>
2021-04-14 21:54:10 +0800

    新增H5学习

---
+++ others/1.gfyt边缘网关代码/EdgeGateway/Javascript/ImgChangeBase64.js
@@ -0,0 +1,9 @@
+function getBase64Image(img) {
+    let canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+    let ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
+    return canvas.toDataURL(ext).split(",")[1];
+}
