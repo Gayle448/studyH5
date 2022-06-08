3022c68a9594bf8cb61bb887527af6f5d7b1d886
misschen <1129133995@qq.com>
2022-06-08 09:53:22 +0800

    备份前提交

---
+++ src/Vue/VueAxios/axios.vue
@@ -0,0 +1,32 @@
+<template lang=''>
+
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios,axios)
+
+export default {
+    data(){
+        return {
+            fileurl:''
+        }
+    }
+},
+methods: {
+    test(){
+        Vue.axios.get(this.fileurl).then((response) => {
+            console.log(response.data);
+        })
+        this.axios.get(this.fileurl).then((response) => {
+            console.log(response.data);
+        })
+        this.$http.get(this.fileurl).then((response) => {
+            console.log(response.data);
+        })
+    }
+},
+</script>
